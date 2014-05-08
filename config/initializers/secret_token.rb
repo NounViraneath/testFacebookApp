c52c6f8e6e3f0126403fd3dea7549d06807097e6
mysql --version <viraneath2010@gmail.com>
2014-05-08 12:34:01 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestFacebookApp::Application.config.secret_key_base = 'f573eee799618179c2fffc956d00f89f2604deff94b83e13c03fda2b25a6e213b024312f1b9f14bbdda69d0b6dfc63144595353816f0a93b7f8a5fc4d044dd18'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestFacebookApp::Application.config.session_store :cookie_store, key: '_testFacebookApp_session'
